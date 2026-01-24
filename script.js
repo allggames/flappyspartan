2e5b11ebf86786b9f82915aca5077c466bd8de21
TINI <agostinaanahi2001@gmail.com>
2026-01-24 18:53:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ const bg = {
 const bird = {
     x: canvas.width / 3, 
     y: canvas.height / 2,
-    w: 180, 
-    h: 180, 
+    w: 230, 
+    h: 230, 
     radius: 25, 
     speed: 0,
     gravity: 0.4,  

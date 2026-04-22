ff439738c5752791b0c15c7a443adbfffb1f4c0c
TINI <agostinaanahi2001@gmail.com>
2026-04-22 19:21:21 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const SITE_CONFIG = {
+    source: 'atenea',
+    chatUrl: 'https://www.casinoatenea.com/',
+    prizes: {
+        '50%': '¡Hola! Jugué a Spartan Glory y gané un BONO del 50% 🛡️, quiero reclamarlo!',
+        '100%': '¡Hola! Jugué a Spartan Glory y gané un BONO del 100% 🛡️, quiero reclamarlo!',
+        '150%': '¡Hola! Jugué a Spartan Glory y gané un BONO del 150% 🛡️, quiero reclamarlo!',
+        '200%': '¡Hola! Jugué a Spartan Glory y gané un BONO del 200% 🛡️, quiero reclamarlo!'
+    }
+};
